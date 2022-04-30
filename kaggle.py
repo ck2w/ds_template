5cec6e2cc4b1e2f4541c4ad51318524bff0941b0
Ken Chen <chken4869@hotmail.com>
2022-04-30 00:45:15 -0400

    kaggle tricks

---
+++ kaggle.py
@@ -0,0 +1,86 @@
+
+
+# reduce memory
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    
+    for col in tqdm(df.columns):
+        col_type = df[col].dtype
+        
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    
+    return df
+
+  
+  
+# transform csv to pickle
+def transform_csv2pickle(path, usecols, dtype):
+    basecols = ['row_id', 'time_id', 'investment_id', 'target']
+    features = [f'f_{i}' for i in range(300)]
+
+    dtypes = {
+        'row_id': 'str',
+        'time_id': 'uint16',
+        'investment_id': 'uint16',
+        'target': 'float32',
+    }
+    for col in features:
+        dtypes[col] = 'float32'
+  
+    train = pd.read_csv(
+        path,
+        usecols=usecols,
+        dtype=dtypes
+    )
+    train = reduce_mem_usage(train)
+    train.to_pickle('train.pkl')
+
+  
+
+# release GPU memory
+import torch
+from GPUtil import showUtilization as gpu_usage
+from numba import cuda
+
+def free_gpu_cache():
+    print("Initial GPU Usage")
+    gpu_usage()                             
+
+    torch.cuda.empty_cache()
+
+    cuda.select_device(0)
+    cuda.close()
+    cuda.select_device(0)
+
+    print("GPU Usage after emptying the cache")
+    gpu_usage()
+
+free_gpu_cache()
